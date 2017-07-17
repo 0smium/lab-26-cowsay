a3e9794aca1720ea96be90e41f1b2d988cbd37c9
0smium <steve.walsh@transfueler.com>
2017-07-17 14:52:20 -0700

    finished scaffold and started cowsay

--- lab-steve/src/main.js
+++ lab-steve/src/main.js
@@ -2,16 +2,43 @@ import './style/main.scss';
 import React from 'react'; //use JSX
 import ReactDom from 'react-dom'; //render JSX
 
-class Navbar extends React.Component {
+import cowsay from 'cowsay-browser';
+import faker from 'faker';
+
+class App extends React.Component {
   constructor(props) {
-    super(props);
-  }
+    super(props)
+    this.state = {
+      title:
+      'Cowsay',
+      count: 0,
+    };
+
+    this.cow = cowsay.say({
+      text : faker.hacker.phrase(),
+    });
+
+    this.handleClick = this.handleClick.bind(this);
+  };
+
+  handleClick(e) {
+    console.log('event', e);
+
+    this.setState((state) => {
+      return {
+        count: state.count + 1
+      };
+    });
+  };
 
   render() {
     return (
-      <header className='hello-navbar'>
-        <h1> lab-26 </h1>
-      </header>
-    );
-  }
-}
+      <div>
+        <button onClick={this.handleClick}> click me </button>
+        <p> {this.cow} </p>
+      </div>
+    )
+  };
+};
+
+ReactDom.render(<App />, document.getElementById('root'))
